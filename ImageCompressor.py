8fb54b946b94c2e0bac709b7ec4ba91b768f4a63
Edu <eduard.colo.llom@gmailcom>
2026-03-28 18:25:18 +0200

    second

--- ImageCompressor.py
+++ ImageCompressor.py
@@ -1,7 +1,7 @@
 
 import os
 import cv2
-from Utils import applyConvolution
+import json
 
 workFolder = "IMAGES"
 
@@ -9,10 +9,13 @@ count = 1
 dictOfInputs = {}
 for file in os.listdir(workFolder):
 
-    image = cv2.imread(os.path.join(workFolder, file))
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image = cv2.resize(image, (100, 100))
-    image = image/255
-    dictOfInputs[file] = image
-    print(count)
-    count += 1
+    if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+        image = cv2.imread(os.path.join(workFolder, file))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(image, (100, 100))
+        image = image/255
+
+        dictOfInputs[file] = image.tolist()
+
+with open("IMAGES/Database.json", "w") as jsonFile:
+    json.dump(dictOfInputs, jsonFile, indent=4)
